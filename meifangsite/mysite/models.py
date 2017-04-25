7f5293f06dc75038369bdb801fb7d56622c90276
lily <515550681@qq.com>
2017-04-25 21:04:45 +0800

    modify InputContent save

--- meifangsite/mysite/models.py
+++ meifangsite/mysite/models.py
@@ -25,13 +25,18 @@ class BaseModel(models.Model):
 
 
 class InputContent(BaseModel):
-    author = models.CharField(max_length=200, verbose_name='作者')
+    author = models.CharField(max_length=200, verbose_name='作者', null=True, blank=True)
     network_platform = models.CharField(max_length=100, verbose_name='网络平台')
     url = models.CharField(max_length=200, verbose_name='文章链接')
 
     def __unicode__(self):
         return u'author={author}, network_platform={network_platform},'\
                 .format(author=self.author, network_platform=self.network_platform)
+
+    def save(self, *args, **kwargs):
+        if not self.author:
+            self.author = self.network_platform
+        super(InputContent, self).save(*args, **kwargs)
 
     class Meta:
         db_table = 'input_content'
