8637935f1c9b13b942f0d4cb898a63179ad34163
lily <515550681@qq.com>
2017-05-04 15:41:26 +0800

    add model EstateWeb, SNSWeb, WeiXin

--- meifangsite/mysite/adminx.py
+++ meifangsite/mysite/adminx.py
@@ -7,7 +7,11 @@ from django.contrib.auth.models import Group, User
 import xadmin
 from xadmin import views
 
-from .models import  InputContent
+from .models import  GovWeb,\
+        EstateWeb,\
+        SNSWeb,\
+        WeiXin
+
 
 logger = logging.getLogger('mysite')
 
@@ -22,26 +26,53 @@ class GlobalSetting(object):
     site_title = u'美房网舆情监控系统'
     site_footer = u'美房网舆情监控系统'
     menu_style = 'default'
-    global_search_models = [ InputContent,]
+    global_search_models = [ GovWeb,]
 
     apps_icons = {
             'mysite': 'fa fa-cloud',
             }
     global_models_icon = {
-            InputContent: 'fa fa-list-alt',
+            GovWeb: 'fa fa-list-alt',
             }
 
 xadmin.sites.site.register(views.CommAdminView, GlobalSetting)
 
 
-class InputContentAdmin(object):
-    list_display = ('id', 'author', 'network_platform', 'url', 'has_crawl')
+class GovWebAdmin(object):
+    list_display = ('id', 'network_platform', 'url')
     list_display_links = ('network_platform', )
-    list_editable = ('has_crawl',)
-    search_fields = ['author', 'network_platform']
+    #list_editable = ('has_crawl',)
+    search_fields = ['network_platform']
     list_export = ()
 
-xadmin.sites.site.register(InputContent, InputContentAdmin)
+xadmin.sites.site.register(GovWeb, GovWebAdmin)
+
+class EstateWebAdmin(object):
+    list_display = ('id', 'network_platform', 'url')
+    list_display_links = ('network_platform', )
+    #list_editable = ('has_crawl',)
+    search_fields = ['network_platform']
+    list_export = ()
+
+xadmin.sites.site.register(EstateWeb, EstateWebAdmin)
+
+class SNSWebAdmin(object):
+    list_display = ('id', 'network_platform', 'url')
+    list_display_links = ('network_platform', )
+    #list_editable = ('has_crawl',)
+    search_fields = ['network_platform']
+    list_export = ()
+
+xadmin.sites.site.register(SNSWeb, SNSWebAdmin)
+
+class WeiXinAdmin(object):
+    list_display = ('id', 'network_platform', 'url')
+    list_display_links = ('network_platform', )
+    #list_editable = ('has_crawl',)
+    search_fields = ['network_platform']
+    list_export = ()
+
+xadmin.sites.site.register(WeiXin, WeiXinAdmin)
 
 xadmin.sites.site.unregister(Group)
 xadmin.sites.site.unregister(User)
--- meifangsite/mysite/models.py
+++ meifangsite/mysite/models.py
@@ -24,11 +24,47 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class InputContent(BaseModel):
-    author = models.CharField(max_length=200, verbose_name='作者', null=True, blank=True)
+class GovWeb(BaseModel):
+    LEVEL_DEFAULT = 999
+
+    title = models.CharField(max_length=200, verbose_name='网页标题', null=True, blank=True)
     network_platform = models.CharField(max_length=100, verbose_name='网络平台')
     url = models.CharField(max_length=200, verbose_name='文章链接')
     has_crawl = models.BooleanField(default=False, verbose_name='是否抓取')
+    has_parser = models.BooleanField(default=False, verbose_name='是否解析')
+    level = models.IntegerField(default=LEVEL_DEFAULT)
+
+    def __unicode__(self):
+        return u'network_platform={network_platform},'\
+                .format(network_platform=self.network_platform)
+
+    #def save(self, *args, **kwargs):
+    #    if not self.author:
+    #        self.author = self.network_platform
+    #    super(GovWeb, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'gov_web'
+        verbose_name = verbose_name_plural = '政府网站'
+
+class EstateWeb(BaseModel):
+
+    network_platform = models.CharField(max_length=100, verbose_name='网络平台')
+    url = models.CharField(max_length=200, verbose_name='文章链接')
+
+    def __unicode__(self):
+        return u'network_platform={network_platform},'\
+                .format(network_platform=self.network_platform)
+
+    class Meta:
+        db_table = 'estate_web'
+        verbose_name = verbose_name_plural = '房产网站'
+
+class SNSWeb(BaseModel):
+
+    author = models.CharField(max_length=200, verbose_name='作者', null=True, blank=True)
+    network_platform = models.CharField(max_length=100, verbose_name='网络平台')
+    url = models.CharField(max_length=200, verbose_name='文章链接')
 
     def __unicode__(self):
         return u'author={author}, network_platform={network_platform},'\
@@ -37,8 +73,27 @@ class InputContent(BaseModel):
     def save(self, *args, **kwargs):
         if not self.author:
             self.author = self.network_platform
-        super(InputContent, self).save(*args, **kwargs)
+        super(SNSWeb, self).save(*args, **kwargs)
 
     class Meta:
-        db_table = 'input_content'
-        verbose_name = verbose_name_plural = '内容详情'
+        db_table = 'sns_web'
+        verbose_name = verbose_name_plural = 'SNS平台'
+
+class WeiXin(BaseModel):
+
+    author = models.CharField(max_length=200, verbose_name='作者', null=True, blank=True)
+    network_platform = models.CharField(max_length=100, verbose_name='网络平台')
+    url = models.CharField(max_length=200, verbose_name='文章链接')
+
+    def __unicode__(self):
+        return u'author={author}, network_platform={network_platform},'\
+                .format(author=self.author, network_platform=self.network_platform)
+
+    def save(self, *args, **kwargs):
+        if not self.author:
+            self.author = self.network_platform
+        super(WeiXin, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'weixin'
+        verbose_name = verbose_name_plural = '微信'
